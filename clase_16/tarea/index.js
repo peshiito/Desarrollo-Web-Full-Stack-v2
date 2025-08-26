c6f3bc724bd9b00a5172ad8871babcc12ec212b2
peshiito <pgbaez0@gmail.com>
2025-08-25 23:40:15 -0300

    udapte punto 2 clase 16 terminado

--- clase_16/tarea/index.js
+++ clase_16/tarea/index.js
@@ -1,13 +1,54 @@
-
-document.querySelectorAll("#registro_multiplicacion").addEventListener('submit', function (e) {
+// PUNTO 1
+document
+  .querySelector("#registro_multiplicacion")
+  .addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const numero = document.querySelector('#numero1').value;
+    const numero = parseInt(document.querySelector("#numero1").value);
+    const tabla = document.querySelector("#tabla_multiplicacion");
 
-    for (let i = 0; i < 11; i++){
-        document.querySelector(
-            '#tabla_multiplicacion'
-          ).textContent = `¡Hola, ${nombre}! Tienes ${edad} años.`;
+    tabla.innerHTML = "";
+
+    for (let i = 1; i <= 10; i++) {
+      let multiplo = numero * i;
+
+      tabla.innerHTML += `<p>${numero} x ${i} = ${multiplo}</p>`;
+    }
+  });
+// PUNTO 2
+
+const array_numeros = [];
+
+//  función para sumar arrays
+function sumarArray(arr) {
+  return arr.reduce((acc, num) => acc + num, 0);
+}
+
+document
+  .querySelector("#registro_sumador")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const numero2 = parseInt(document.querySelector("#numero2").value);
+
+    if (!isNaN(numero2) && array_numeros.length < 5) {
+      array_numeros.push(numero2);
     }
 
-});
+    const tabla_sumadora = document.querySelector("#mensaje_suma");
+
+    if (array_numeros.length === 5) {
+      const suma = sumarArray(array_numeros);
+      tabla_sumadora.innerHTML = `
+          <p>Números ingresados: ${array_numeros.join(", ")}</p>
+          <p><strong>Suma total: ${suma}</strong></p>
+        `;
+    } else {
+      tabla_sumadora.innerHTML = `
+          <p>Números ingresados: ${array_numeros.join(", ")}</p>
+          <p>Faltan ${5 - array_numeros.length} números por ingresar</p>
+        `;
+    }
+  });
+
+// PUNTO 3
